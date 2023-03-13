40f8871dc1b1187d221584c67ebb91d144a0fd48
Lidor <lidororanim@gmail.com>
2023-03-13 17:05:01 +0200

    update reply message

--- bot/app.py
+++ bot/app.py
@@ -78,7 +78,7 @@ class YoutubeBot(Bot):
                     }
                 )
                 logger.info(f'msg {response.get("MessageId")} has been sent to queue')
-                self.send_text('your for your massage its being process....')
+                self.send_text('your massage its being process....')
             except ClientError as error:
                 logger.error(error)
                 self.send_text('Something went wrong, please try again...')
