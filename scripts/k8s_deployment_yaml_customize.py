8094cf0da41739502d44cc40724256fef4558b76
Lidor <lidororanim@gmail.com>
2023-03-13 09:21:22 +0200

    added script to deploy k0s

---
+++ scripts/k8s_deployment_yaml_customize.py
@@ -0,0 +1,22 @@
+import yaml
+import os
+
+
+env = os.getenv("APP_ENV")
+image = os.getenv("BOT_IMAGE_NAME") if os.getenv("BOT_IMAGE_NAME") else os.getenv("WORKER_IMAGE_NAME")
+worker_image = os.getenv("WORKER_IMAGE_NAME")
+file_to_save = f'{os.getenv("K8S_DEPLOYMENT_FILE")}'
+file_to_open = f'{os.getenv("K8S_YAML_TO_EDIT")}'
+
+with open(file_to_open) as file:
+    y = yaml.safe_load(file)
+    y["metadata"]["labels"]["env"] = env
+    y["spec"]["selector"]["matchLabels"]["env"] = env
+    y["spec"]["template"]["metadata"]["labels"]["env"] = env
+    y["spec"]["template"]["spec"]["containers"][0]["env"][0]["value"] = env
+    y["spec"]["template"]["spec"]["containers"][0]["image"] = image
+
+
+
+with open(file_to_save, 'w') as deploy:
+    yaml.dump(y, deploy)
